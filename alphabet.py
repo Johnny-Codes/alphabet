6c38342449f2f4da8768670ed3756590b25a2ce0
Paul Johns <35434386+Johnny-Codes@users.noreply.github.com>
2018-02-02 13:36:37 -0500

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -5,7 +5,6 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 
 numbers = [1, 2, 3, 4, 5]
 
-
 # 1 = addition, 2 = subtraction
 operators = [1, 2]
 
@@ -33,7 +32,15 @@ def rand_num():
     if c == 1:
         d = a + b
         print(f"\nWhat is {a} + {b}?")
-        x = int(input("\n>>> "))
+        while True:
+            try:
+                x = int(input("\n>>> "))
+                break
+            except ValueError:
+                print("\nThat's no number! Try again")
+                print(f"\nWhat is {a} + {b}?")
+            except KeyboardInterrupt:
+                start()
         if d == x:
             print(f"\n{random.choice(win)}")
             rand_num()
@@ -43,51 +50,54 @@ def rand_num():
             rand_num()
     if c == 2:
         # don't want negative numbers for an answer
-        # you can delete this second layer if statement...
+        # you can delete this layer if/else statement...
         # if you want negative numbers as answers
         if a >= b:
             d = a - b
             print(f"\nWhat is {a} - {b}?")
-            x = int(input("\n>>> "))
+            while True:
+                try:
+                    x = int(input("\n>>> "))
+                    break
+                except ValueError:
+                    print("\nThat's no number! Try again")
+                    print(f"\nWhat is {a} - {b}?")
             if d == x:
                 print(f"\n{random.choice(win)}")
                 rand_num()
-            elif ValueError in d:
-                pass
             else:
                 print(f"\n{random.choice(oops)}")
                 print("The correct answer is", d)
                 rand_num()
+        # delete below here if you want negative answers
         else:
-            d = b - a
+            print("b > a")
             print(f"\nWhat is {b} - {a}?")
-            x = int(input("\n>>> "))
+            while True:
+                try:
+                    x = int(input("\n>>> "))
+                    break
+                except ValueError:
+                    print("\nThat's no number! Try again")
+                    print(f"\nWhat is {b} - {a}?")
             if d == x:
                 print(f"\n{random.choice(win)}")
                 rand_num()
-            elif ValueError in d:
-                pass
             else:
                 print(f"\n{random.choice(oops)}")
                 print("The correct answer is", d)
                 rand_num()
 
 def start():
-    x = int(input(("Do you want to play with letters or numbers\nPress 1 for letters and 2 for numbers")))
+    x = int(input(("Do you want to play with letters or numbers\nPress 1 for letters and 2 for numbers\n>>> ")))
     if x == 1:
         rand_let()
-    else:
+    elif x == 2:
         rand_num()
+    else:
+        start()
 
 
 start()
-#rand_let()
-#rand_num()
 
-
-
-
-
-### Practice Code Below ###
-#print(random.choice(alphabet))
-#tkinter? GUI this shit so it looks good
+#tkinter or wxPython? GUI this so it looks good
